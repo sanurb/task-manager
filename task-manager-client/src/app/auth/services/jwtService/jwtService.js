c6322e19dd5b5844606289816c99ad2b1ed585d6
Santiago Urbano <david.urbano@uao.edu.co>
2023-09-06 08:34:57 -0500

    fix(jwtService.js): update createUser method to use new registration endpoint fix(jwtService.js): update signInWithEmailAndPassword method to use new login endpoint

--- task-manager-client/src/app/auth/services/jwtService/jwtService.js
+++ task-manager-client/src/app/auth/services/jwtService/jwtService.js
@@ -49,36 +49,31 @@ class JwtService extends FuseUtils.EventEmitter {
 
   createUser = (data) => {
     return new Promise((resolve, reject) => {
-      axios.post(jwtServiceConfig.signUp, data).then((response) => {
-        if (response.data.user) {
-          this.setSession(response.data.access_token);
-          resolve(response.data.user);
-          this.emit('onLogin', response.data.user);
-        } else {
-          reject(response.data.error);
-        }
-      });
+        axios.post('http://127.0.0.1:8090/auth/register', data).then((response) => {
+            if (response.data.user) {
+                this.setSession(response.data.access_token);
+                resolve(response.data.user);
+                this.emit('onLogin', response.data.user);
+            } else {
+                reject(response.data.error);
+            }
+        });
     });
   };
 
   signInWithEmailAndPassword = (email, password) => {
     return new Promise((resolve, reject) => {
-      axios
-        .get(jwtServiceConfig.signIn, {
-          data: {
-            email,
-            password,
-          },
-        })
-        .then((response) => {
-          if (response.data.user) {
-            this.setSession(response.data.access_token);
-            resolve(response.data.user);
-            this.emit('onLogin', response.data.user);
-          } else {
-            reject(response.data.error);
-          }
-        });
+        axios
+            .post('http://127.0.0.1:8090/auth/login', { email, password })
+            .then((response) => {
+                if (response.data.user) {
+                    this.setSession(response.data.access_token);
+                    resolve(response.data.user);
+                    this.emit('onLogin', response.data.user);
+                } else {
+                    reject(response.data.error);
+                }
+            });
     });
   };
 
