84d14c8c11e72c5683b4afb69f5af1fa619e3a6e
Santiago Urbano <david.urbano@uao.edu.co>
2023-09-06 17:19:27 -0500

    fix(AuthResponse.java): add import statement for User class fix(AuthResponse.java): change token field to access_token field fix(AuthService.java): remove todo comment fix(AuthService.java): change token field to access_token field feat(AuthService.java): add user field to AuthResponse object

--- task-manager-api/src/main/java/com/taskmanager/taskmanagerapi/Auth/AuthService.java
+++ task-manager-api/src/main/java/com/taskmanager/taskmanagerapi/Auth/AuthService.java
@@ -26,14 +26,14 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
-    // todo: check why is a error when giving the correct credentials
     public AuthResponse login(LoginRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
         UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
         String token = jwtService.getToken(user);
 
         return AuthResponse.builder()
-                .token(token)
+                .user((User) user)
+                .access_token(token)
                 .build();
 
     }
@@ -62,7 +62,7 @@ public class AuthService {
 
         return AuthResponse.builder()
                 // todo generar token
-                .token(jwtService.getToken(user))
+                .access_token(jwtService.getToken(user))
                 .build();
     }
 }
