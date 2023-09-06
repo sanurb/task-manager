aa41f4c84d0c7548ef5b80df5a62de9bcc14c90e
Santiago Urbano <david.urbano@uao.edu.co>
2023-09-06 08:37:22 -0500

    refactor(AuthController.java): remove unused imports feat(AuthController.java): add CrossOrigin annotation to login endpoint

--- task-manager-api/src/main/java/com/taskmanager/taskmanagerapi/Auth/AuthController.java
+++ task-manager-api/src/main/java/com/taskmanager/taskmanagerapi/Auth/AuthController.java
@@ -2,10 +2,7 @@ package com.taskmanager.taskmanagerapi.Auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
@@ -16,6 +13,7 @@ public class AuthController {
 
     private final AuthService authService;
     @PostMapping(value = "login")
+    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
 
         return ResponseEntity.ok(authService.login(request));
