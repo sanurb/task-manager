073e154e3824630227fc7b7ba31c06e55fd7d0ff
Santiago Urbano <david.urbano@uao.edu.co>
2023-09-06 15:24:23 -0500

    refactor(SignInPage.js): update welcome message and description text feat(SignInPage.js): update join us message

--- task-manager-client/src/app/main/sign-in/SignInPage.js
+++ task-manager-client/src/app/main/sign-in/SignInPage.js
@@ -233,12 +233,11 @@ function SignInPage() {
 
         <div className="z-10 relative w-full max-w-2xl">
           <div className="text-7xl font-bold leading-none text-gray-100">
-            <div>Welcome to</div>
-            <div>our community</div>
+            <div>Unleash Your</div>
+            <div>Productivity</div>
           </div>
           <div className="mt-24 text-lg tracking-tight leading-6 text-gray-400">
-            Fuse helps developers to build organized and well coded dashboards full of beautiful and
-            rich modules. Join us and start building your application today.
+            Discover the power of seamless project management with our platform. Organize tasks, collaborate with teams, and achieve your goals efficiently.
           </div>
           <div className="flex items-center mt-32">
             <AvatarGroup
@@ -255,7 +254,7 @@ function SignInPage() {
             </AvatarGroup>
 
             <div className="ml-16 font-medium tracking-tight text-gray-400">
-              More than 17k people joined us, it's your turn
+              Thousands of teams trust us. Dive in today.
             </div>
           </div>
         </div>
